fc24ca4d90489907008d40e83215b301c9e352cb
sanboxrunner <jjspira@gmail.com>
2018-09-13 19:50:05 -0400

    Updated for localised types

--- index.ts
+++ index.ts
@@ -631,8 +631,9 @@ export namespace Resource {
     isDefaultView: boolean;
 
     /** A code, likely used for adding localizations. */
-    localisedFolderName: string;
+    localisedFolderName: localisedNames;
   }
+  type localisedNames = "" | "ResourceTree_Fonts" | "ResourceTree_Scripts" | "ResourceTree_Notes" | "ResourceTree_Sounds" | "ResourceTree_Tilesets" | "ResourceTree_Configs" | "ResourceTree_Paths" | "ResourceTree_Sprites" | "ResourceTree_IncludedFiles" | "ResourceTree_Extensions" | "ResourceTree_Objects" | "ResourceTree_Options" | "ResourceTree_Timelines" | "ResourceTree_Shaders" | "ResourceTree_Rooms"
 
   export interface Tileset extends BaseResource {
     /** Internal resource type descriptor */
